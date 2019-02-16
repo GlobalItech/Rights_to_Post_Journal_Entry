d6b9e2591c96be818a001cb3ee7566cb1c1e8998
GlobalItech <30366468+GlobalItech@users.noreply.github.com>
2019-02-16 12:15:05 +0500

    Add files via upload

---
+++ entry_post/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'Entry Post',
+    'author' : 'iTechresources',
+    'website' : 'www.itechresoureces.com',
+    'description' : 'After validation inovice should be in Unposted state in Journal',
+    'depends' : ['account', 'sale'],
+    'version' : '1.0',
+    'data' :[
+                'views/post_approval.xml',
+                'security/invoice_security.xml'
+            ],
+    'installable' : True,
+    'price':'15.0',
+    'currency': 'EUR',
+}
---
+++ entry_post/models/invoiceAsUnposted.py
@@ -0,0 +1,183 @@
+
+from odoo import models, api, fields
+from odoo.exceptions import RedirectWarning, UserError, ValidationError
+
+class accountmove(models.Model):
+    _inherit = "account.move"
+     
+    @api.multi
+    def post_custom(self):
+        invoice = self._context.get('invoice', False)
+        self._post_validate()
+        for move in self:
+            move.line_ids.create_analytic_lines()
+            if move.name == '/':
+                new_name = False
+                journal = move.journal_id
+ 
+                if invoice and invoice.move_name and invoice.move_name != '/':
+                    new_name = invoice.move_name
+                else:
+                    if journal.sequence_id:
+                        # If invoice is actually refund and journal has a refund_sequence then use that one or use the regular one
+                        sequence = journal.sequence_id
+                        if invoice and invoice.type in ['out_refund', 'in_refund'] and journal.refund_sequence:
+                            if not journal.refund_sequence_id:
+                                raise UserError(_('Please define a sequence for the refunds'))
+                            sequence = journal.refund_sequence_id
+                                                             
+                        new_name = sequence.with_context(ir_sequence_date=move.date).next_by_id()
+                    else:
+                        raise UserError(_('Please define a sequence on the journal.'))
+ 
+                if new_name:
+                    move.name = new_name
+        return self.write({'state': 'draft'})
+     
+class accountInvoice(models.Model):
+    _inherit = "account.invoice"
+     
+    @api.multi
+    def action_move_create(self):
+        """ Creates invoice related analytics and financial move lines """
+        account_move = self.env['account.move']
+ 
+        for inv in self:
+            if not inv.journal_id.sequence_id:
+                raise UserError(_('Please define sequence on the journal related to this invoice.'))
+            if not inv.invoice_line_ids:
+                raise UserError(_('Please create some invoice lines.'))
+            if inv.move_id:
+                continue
+ 
+            ctx = dict(self._context, lang=inv.partner_id.lang)
+ 
+            if not inv.date_invoice:
+                inv.with_context(ctx).write({'date_invoice': fields.Date.context_today(self)})
+            date_invoice = inv.date_invoice
+            company_currency = inv.company_id.currency_id
+ 
+            # create move lines (one per invoice line + eventual taxes and analytic lines)
+            iml = inv.invoice_line_move_line_get()
+            iml += inv.tax_line_move_line_get()
+ 
+            diff_currency = inv.currency_id != company_currency
+            # create one move line for the total and possibly adjust the other lines amount
+            total, total_currency, iml = inv.with_context(ctx).compute_invoice_totals(company_currency, iml)
+ 
+            name = inv.name or '/'
+            if inv.payment_term_id:
+                totlines = inv.with_context(ctx).payment_term_id.with_context(currency_id=company_currency.id).compute(total, date_invoice)[0]
+                res_amount_currency = total_currency
+                ctx['date'] = date_invoice
+                for i, t in enumerate(totlines):
+                    if inv.currency_id != company_currency:
+                        amount_currency = company_currency.with_context(ctx).compute(t[1], inv.currency_id)
+                    else:
+                        amount_currency = False
+ 
+                    # last line: add the diff
+                    res_amount_currency -= amount_currency or 0
+                    if i + 1 == len(totlines):
+                        amount_currency += res_amount_currency
+ 
+                    iml.append({
+                        'type': 'dest',
+                        'name': name,
+                        'price': t[1],
+                        'account_id': inv.account_id.id,
+                        'date_maturity': t[0],
+                        'amount_currency': diff_currency and amount_currency,
+                        'currency_id': diff_currency and inv.currency_id.id,
+                        'invoice_id': inv.id
+                    })
+            else:
+                iml.append({
+                    'type': 'dest',
+                    'name': name,
+                    'price': total,
+                    'account_id': inv.account_id.id,
+                    'date_maturity': inv.date_due,
+                    'amount_currency': diff_currency and total_currency,
+                    'currency_id': diff_currency and inv.currency_id.id,
+                    'invoice_id': inv.id
+                })
+            part = self.env['res.partner']._find_accounting_partner(inv.partner_id)
+            line = [(0, 0, self.line_get_convert(l, part.id)) for l in iml]
+            line = inv.group_lines(iml, line)
+ 
+            journal = inv.journal_id.with_context(ctx)
+            line = inv.finalize_invoice_move_lines(line)
+ 
+            date = inv.date or date_invoice
+            move_vals = {
+                'ref': inv.reference,
+                'line_ids': line,
+                'journal_id': journal.id,
+                'date': date,
+                'narration': inv.comment,
+            }
+            ctx['company_id'] = inv.company_id.id
+            ctx['invoice'] = inv
+            ctx_nolang = ctx.copy()
+            ctx_nolang.pop('lang', None)
+            move = account_move.with_context(ctx_nolang).create(move_vals)
+            # Pass invoice in context in method post: used if you want to get the same
+            # account move reference when creating the same invoice after a cancelled one:
+            move.post_custom()
+            # make the invoice point to that move
+            vals = {
+                'move_id': move.id,
+                'date': date,
+                'move_name': move.name,
+            }
+            inv.with_context(ctx).write(vals)
+        return True
+ 
+class accountPayment(models.Model):
+    _inherit = "account.payment"
+     
+    @api.multi
+    def post(self):
+        """ Create the journal items for the payment and update the payment's state to 'posted'.
+            A journal entry is created containing an item in the source liquidity account (selected journal's default_debit or default_credit)
+            and another in the destination reconciliable account (see _compute_destination_account_id).
+            If invoice_ids is not empty, there will be one reconciliable move line per invoice to reconcile with.
+            If the payment is a transfer, a second journal entry is created in the destination journal to receive money from the transfer account.
+        """
+        for rec in self:
+ 
+            if rec.state != 'draft':
+                raise UserError(_("Only a draft payment can be posted. Trying to post a payment in state %s.") % rec.state)
+ 
+            if any(inv.state != 'open' for inv in rec.invoice_ids):
+                raise ValidationError(_("The payment cannot be processed because the invoice is not open!"))
+ 
+            # Use the right sequence to set the name
+            if rec.payment_type == 'transfer':
+                sequence_code = 'account.payment.transfer'
+            else:
+                if rec.partner_type == 'customer':
+                    if rec.payment_type == 'inbound':
+                        sequence_code = 'account.payment.customer.invoice'
+                    if rec.payment_type == 'outbound':
+                        sequence_code = 'account.payment.customer.refund'
+                if rec.partner_type == 'supplier':
+                    if rec.payment_type == 'inbound':
+                        sequence_code = 'account.payment.supplier.refund'
+                    if rec.payment_type == 'outbound':
+                        sequence_code = 'account.payment.supplier.invoice'
+            rec.name = self.env['ir.sequence'].with_context(ir_sequence_date=rec.payment_date).next_by_code(sequence_code)
+ 
+            # Create the journal entry
+            amount = rec.amount * (rec.payment_type in ('outbound', 'transfer') and 1 or -1)
+            move = rec._create_payment_entry(amount)
+ 
+            # In case of a transfer, the first journal entry created debited the source liquidity account and credited
+            # the transfer account. Now we debit the transfer account and credit the destination liquidity account.
+            if rec.payment_type == 'transfer':
+                transfer_credit_aml = move.line_ids.filtered(lambda r: r.account_id == rec.company_id.transfer_account_id)
+                transfer_debit_aml = rec._create_transfer_entry(amount)
+                (transfer_credit_aml + transfer_debit_aml).reconcile()
+ 
+            rec.write({'state': 'draft', 'move_name': move.name})
